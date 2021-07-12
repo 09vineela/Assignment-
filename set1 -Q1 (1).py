5abe05686e4af26956dcc83116eba7b7573d8d35
Vineela <86728573+09vineela@users.noreply.github.com>
2021-07-12 08:23:17 -0700

    Add files via upload

---
+++ set1 -Q1 (1).py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+''' What is the output of the following code?
+nums = set([1,1,2,3,3,3,4,4])
+print(len(nums))'''
+
+
+# In[3]:
+
+
+nums = set([1,1,2,3,3,3,4,4])
+print(len(nums))
+
